a967374d8d9e0989e61833fff0dc427df56dae20
Zeel Makwana <zeelmakwana55@gmail.com>
2025-11-06 20:48:03 +0530

    Add build_files.sh for Vercel build

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,1 @@
-﻿#!/bin/bash
-set -e
-echo "Python version:"
-python --version
-
-# Make sure pip is up to date
-python -m pip install --upgrade pip
-
-# Install python dependencies from requirements.txt
-pip install -r requirements.txt
-
-# Collect static files into staticfiles/ (used by vercel.json)
-python manage.py collectstatic --noinput
-
-# Try to run migrations, but don't fail the build if DB isn't configured
-python manage.py migrate --noinput || true
-
-echo "Build finished"
+﻿
